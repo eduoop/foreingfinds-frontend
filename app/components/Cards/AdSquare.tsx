18a3c6787e933285858399024ccf7ff666f538f5
Eduardo <eduardo.moraes@clicksoft.com.br>
2023-09-11 19:35:09 -0300

    fix: ad card redirect

--- app/components/Cards/AdSquare.tsx
+++ app/components/Cards/AdSquare.tsx
@@ -43,29 +43,31 @@ const AdSquare = ({ ad }: Props) => {
   }
 
   return (
-    <Link href={`/ad/${ad.id}`} className='flex flex-col w-full h-full bg-white border-2 border-weakGray gap-2 shadow-lg cursor-pointer rounded-md'>
+    <div className='flex flex-col w-full h-full bg-white border-2 border-weakGray gap-2 shadow-lg cursor-pointer rounded-md'>
       <div className='w-auto tablet:w-[100%] h-[100%] tablet:h-[200px] group'>
         <StyledCarousel
           responsive={responsive}
           infinite={true}
           className='h-full rounded-t-md'
-          customLeftArrow={<SlArrowLeft className="absolute text-xl top-1/2 left-4 cursor-pointer text-white hidden group-hover:block" />}
-          customRightArrow={<SlArrowRight className="absolute text-xl top-1/2 right-4 cursor-pointer text-white hidden group-hover:block" />}
+          customLeftArrow={<SlArrowLeft className="absolute h-full text-xl top-0 left-4 cursor-pointer text-white hidden group-hover:block" />}
+          customRightArrow={<SlArrowRight className="absolute h-full text-xl top-0 right-4 cursor-pointer text-white hidden group-hover:block" />}
           renderArrowsWhenDisabled
         >
           {ad.files.map((adImage) => (
-            <img
-              draggable={false}
-              className='object-cover w-full h-full select-none rounded-t-md'
-              style={{ pointerEvents: "none" }}
-              src={adImage.file_url}
-              alt="ad image"
-            />
+            <Link href={`/ad/${ad.id}`} className='flex flex-col w-full h-full bg-white border-2 border-weakGray gap-2 shadow-lg cursor-pointer rounded-md'>
+              <img
+                draggable={false}
+                className='object-cover w-full h-full select-none rounded-t-md'
+                style={{ pointerEvents: "none" }}
+                src={adImage.file_url}
+                alt="ad image"
+              />
+            </Link>
           ))}
         </StyledCarousel>
       </div>
 
-      <div className='flex flex-col p-3'>
+      <Link href={`/ad/${ad.id}`} className='flex flex-col p-3'>
         <div>
           {ad.previous_price ?
             <small className='text-gray-500 line-through'>{FilterIntegerValueToReal(ad.previous_price)}</small>
@@ -75,8 +77,8 @@ const AdSquare = ({ ad }: Props) => {
           <h3 className='text-primaryGraffiti text-2xl leading-[22px]'>{FilterIntegerValueToReal(ad.price)}</h3>
         </div>
         <h2 className='text-primaryGraffiti text-md font-light'>{ad.title}</h2>
-      </div>
-    </Link>
+      </Link>
+    </div>
   )
 }
 

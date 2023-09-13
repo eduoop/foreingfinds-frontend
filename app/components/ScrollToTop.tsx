e113ab972d49104c87a3c1d8d729d8b0a92b4a80
Eduardo <eduardo.moraes@clicksoft.com.br>
2023-09-13 20:21:28 -0300

    fix: remove unused var

--- app/components/ScrollToTop.tsx
+++ app/components/ScrollToTop.tsx
@@ -4,7 +4,6 @@ import { usePathname } from 'next/navigation';
 
 export default function ScrollToTop() {
   const pathname  = usePathname();
-  const token = localStorage.getItem("authToken")
 
   useEffect(() => {
     window.scrollTo(0, 0);

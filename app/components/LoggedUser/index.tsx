c1357021dfed4f7228b4ad1ae9bcf645864458c1
Eduardo <eduardo.moraes@clicksoft.com.br>
2023-09-15 11:33:20 -0300

    feat: resize logged user avatar

--- app/components/LoggedUser/index.tsx
+++ app/components/LoggedUser/index.tsx
@@ -20,14 +20,14 @@ const LoggedUser = () => {
           <div className='rounded-full py-1 px-1 flex items-center border-neutralBlack border cursor-pointer group duration-200 hover:border-primaryOrange gap-2'>
             {user.avatar ?
               <div className='flex gap-2 items-center group-hover:text-primaryOrange duration-200'>
-                <div className='w-[50px] h-[50px] tablet:w-[32px] tablet:h-[32px]'>
+                <div className='w-[40px] h-[40px] tablet:w-[32px] tablet:h-[32px]'>
                   <img src={user.avatar.file_url} className='h-full w-full object-cover rounded-full' />
                 </div>
                 <h2 className='font-roboto max-w-[200px] overflow-hidden hidden tablet:block'>{FilterFistName(userSurname)}</h2>
               </div>
               :
               <div className='flex gap-2 items-center group-hover:text-primaryOrange duration-200'>
-                <div className='w-[32px] h-[32px] flex items-center justify-center bg-weakGray rounded-full'>
+                <div className='w-[40px] h-[40px] tablet:w-[32px] tablet:h-[32px] flex items-center justify-center bg-weakGray rounded-full'>
                   <FaUserLarge className="w-[25px] text-neutralBlack" />
                 </div>
                 <h2 className='font-roboto max-w-[200px] overflow-hidden hidden tablet:block'>{FilterFistName(userSurname)}</h2>
